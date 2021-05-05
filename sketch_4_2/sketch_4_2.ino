c68ba5ea2fccc9cd0d7eb9bf33aed730acfd1c67
zisa <zisaboy@gmail.com>
2021-05-05 15:36:44 +0800

    add example 4_2

---
+++ sketch_4_2/sketch_4_2.ino
@@ -0,0 +1,28 @@
+//機械開關彈跳 bouncing
+
+const byte LED =13;
+const byte SW = 2;
+bool lastState = LOW;
+bool toggle = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+  pinMode(SW,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bool b1 = digitalRead(SW);
+  if(b1)
+  {
+    lastState = b1;
+  }
+
+  if(b1 != lastState)
+  {
+    toggle = !toggle;
+    digitalWrite(LED,toggle);
+    lastState = LOW;
+  }
+}
