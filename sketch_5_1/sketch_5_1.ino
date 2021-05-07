1d73bf91aa2e57ff918d0c8adb44d52ec5f1c163
zisa <zisaboy@gmail.com>
2021-05-07 17:12:03 +0800

    serial

---
+++ sketch_5_1/sketch_5_1.ino
@@ -0,0 +1,14 @@
+byte ledPin =13;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello");
+  Serial.print("\tLED pin is: ");
+  Serial.print("ledPin");
+  Serial.print("\nBYE!");  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
